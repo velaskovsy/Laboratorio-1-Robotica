7f81b7c108b2885a68bb8dcf145a4b63cbe00a35
velaskovsy <bvelasquezfarias@gmail.com>
2026-04-01 15:40:50 -0300

    Controlador actualizado para hacer variaciones en la trayectoria mediante perturbaciones, si se quiere ver el controlador anterior ir al commit previo a este

--- controllers/ControladorRobotLab1/ControladorRobotLab1.py
+++ controllers/ControladorRobotLab1/ControladorRobotLab1.py
@@ -1,4 +1,5 @@
 """ControladorRobotLab1 controller."""
+import random
 from controller import Robot
 
 robot = Robot()
@@ -8,9 +9,16 @@ timestep = int(robot.getBasicTimeStep())
 left_motor = robot.getDevice('left wheel motor')
 right_motor = robot.getDevice('right wheel motor')
 
+# monitorear sensor encoder
+left_sensor = robot.getDevice('left wheel sensor')
+right_sensor = robot.getDevice('right wheel sensor')
+
 # Esto es para que el robot tenga velocidad infinita
 left_motor.setPosition(float('inf'))
 right_motor.setPosition(float('inf'))
+
+left_sensor.enable(timestep)
+right_sensor.enable(timestep)
 
 # Main loop:
 L = 0.052 # Distancia entre las ruedas
@@ -22,6 +30,14 @@ while robot.step(timestep) != -1:
     vl = v + (w * L) / 2 # velocidad en el motor izquierdo
     vr = v - (w * L) / 2 # velocidad en el motor derecho
     
-    left_motor.setVelocity(vl)
-    right_motor.setVelocity(vr)
+    perturbacion_l = random.uniform(-5, 5)
+    perturbacion_r = random.uniform(-5, 5)
+    
+    left_motor.setVelocity(vl + perturbacion_l)
+    right_motor.setVelocity(vr + perturbacion_r)
+    
+    pos_izq = left_sensor.getValue()
+    pos_der = right_sensor.getValue()
+    
+    print(f"Encoder de la Izquierda: {pos_izq:.2f} - Encoder de la Derecha: {pos_der:.2f}")
     pass
